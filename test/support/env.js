fae9de99d2e3ea78863faeb18a83c74274623766
Darius Riggins <simovate@gmail.com>
2014-07-15 08:16:12 -0700

    Move files around to support travis builds

--- test/support/env.js
+++
@@ -1,7 +0,0 @@
-if (!process.env.NODE_ENV){
-  require('config').db.host = '127.0.0.1';
-  require('config').db.user = 'postgres';
-  require('config').db.password = null;
-}
-
-process.env.NODE_ENV = 'testing';
