55a87c0d85f8ea1c964518ff581f6d00c4e23944
Bryce Jacobs <popasquat89@gmail.com>
2014-08-01 13:50:55 -0500

    indent fix for jsdoc

--- backend/controllers/stats.js
+++ backend/controllers/stats.js
@@ -39,10 +39,9 @@ function leafAverage (leaf, meta) {
  * Will traverse a data tree from the bottom to the top, applying a set of functions
  * to each leaf along the way. 
  *
- * @param 	{[Object.<String, Function>]} fns 	An object containing an array of objects with a field name and function, 
- *                        	used for attaching the calculated data to each leaf.
- * @param 	{Object.<Category>} leaf 	The current leaf that is being worked on.
- * @param 	{Object} meta 	Data that can be accessed from each leaf being worked on, used for calculating averages.
+ * @param	{[Object.<String, Function>]}	fns    An object containing an array of objects with a * field name and function, used for attaching the calculated data to each leaf.
+ * @param 	{Object.<Category>}    leaf    The current leaf that is being worked on.
+ * @param 	{Object} meta 	Data that can be accessed from each leaf being worked on, used for    * calculating averages.
  * @return 	{Object} Returns a category data tree in the parent-child format with calculated statistics on each leaf.
  */
 function treeParser (fns, leaf, meta) {
