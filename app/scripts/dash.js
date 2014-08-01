3bbcb443ae2fa861fc30cf48f10b2690ec29f1bc
Bryce Jacobs <popasquat89@gmail.com>
2014-08-01 15:05:09 -0500

    move rendering of dash items in dash.js

--- app/scripts/dash.js
+++ app/scripts/dash.js
@@ -1,4 +1,4 @@
-(function (window){
+(function (){
     var querySelector = document.querySelector.bind(document);
 
     var trainingName = querySelector('.course-name');
@@ -7,7 +7,17 @@
     var providerName = querySelector('.provider-name');
     var providerBio = querySelector('.provider-bio');
 
+    var courseCount = querySelector('.course-count .dash-text');
+    var passCount = querySelector('.exam-passcount .dash-text');
+    var failCount = querySelector('.exam-failcount .dash-text');
+    var examAverage = querySelector('.exam-average .dash-text');
+
     function dataChange (data){
+        courseCount.innerHTML = '1';
+        passCount.innerHTML = '8';
+        failCount.innerHTML = '3';
+        examAverage.innerHTML = '88%';
+
         trainingName.innerHTML = data.company.name;
         trainingDescription.innerHTML = data.course.description;
 
@@ -16,4 +26,4 @@
     }
 
     window.Trainify.attachCourseDataListener(dataChange);
-})(window);
+})();
