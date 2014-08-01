05a7e15a735b1d42eeb4c309c36461507bf0a8a9
Bryce Jacobs <popasquat89@gmail.com>
2014-08-01 17:36:53 -0500

    change dash script to non-blocking

--- test/controllers/dash.spec.js
+++ test/controllers/dash.spec.js
@@ -29,38 +29,17 @@ describe('dash controller', function(){
     });
 
     it('should retrieve the dash page for a training course', function(done){
-      var companyFields = {
-        bio: 'This is a company bio',
-        name: 'This is a company name'
-      };
-      var trainingFields = {
-        description: 'This is the training description.',
-        name: 'This is the training name.'
-      };
-
-      var companyName = new RegExp('<span class=\"name\">Provided By ' + company.name + '</span>');
-      var companyBio = new RegExp('<p class="bio">' + company.bio + '</p>');
-      var trainingName = new RegExp('<span class=\"name\">' + training.name + '</span>');
-      var trainingDescription = new RegExp('<p class=\"description\">' + training.description + '</p>')
+      var companyName = new RegExp('<span class=\"provider-name\">');
+      var companyBio = new RegExp('<p class="provider-bio">');
+      var trainingName = new RegExp('<span class=\"course-name\">');
+      var trainingDescription = new RegExp('<p class=\"course-description\">')
       
-      sequelize.transaction(function(t){
-        Company.create(companyFields, { transaction: t }).success(function (company){
-          
-          trainingFields.companyId = company.id;
-          Training.create(trainingFields, { transaction: t }).success(function(training){
-            request(global.app)
-              .get('/dash')
-              .expect(companyName)
-              .expect(companyBio)
-              .expect(trainingName)
-              .expect(200, trainingDescription, function(){
-                t.rollback().success(function(){done();});
-              });
-          }).failure(function(){
-            t.rollback.success(function(){done();})
-          });      
-        });
-      });
+      request(global.app)
+        .get('/dash')
+        .expect(companyName)
+        .expect(companyBio)
+        .expect(trainingName)
+        .expect(200, trainingDescription);
     });
 
   }); // describe('/dash')
