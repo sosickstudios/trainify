b0dc0d34fdd4e085d63428c4c922b7d8a01bdc18
Bryce Jacobs <popasquat89@gmail.com>
2014-09-16 17:10:13 -0500

    exercise - add legal statement to bottom

--- backend/models/training.js
+++ backend/models/training.js
@@ -2,11 +2,12 @@ var Sequelize = require('sequelize');
 var sequelize = require('../plugins/db');
 
 module.exports = sequelize.define('training', {
-	id:                    { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-	description:           { type: Sequelize.TEXT },
-	logo:                  { type: Sequelize.STRING },
-	name:                  { type: Sequelize.TEXT },
-    practiceExamTotal:     { type: Sequelize.INTEGER },
-    structuredExamTotal:   { type: Sequelize.INTEGER },
-    cost:                  { type: Sequelize.INTEGER }
+    id:                     { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+    description:            { type: Sequelize.TEXT },
+    legal:                  { type: Sequelize.TEXT},
+    logo:                   { type: Sequelize.STRING },
+    name:                   { type: Sequelize.TEXT },
+    practiceExamTotal:      { type: Sequelize.INTEGER },
+    structuredExamTotal:    { type: Sequelize.INTEGER },
+    cost:                   { type: Sequelize.INTEGER }
 });
