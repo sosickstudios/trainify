2f11067e34464aa0224f12835159f1fb4f5f2b52
Bryce Jacobs <popasquat89@gmail.com>
2014-09-23 13:34:05 -0500

    config - removed unnecessary overwrites

--- config/production.js
+++ config/production.js
@@ -9,19 +9,7 @@ module.exports = {
       secretKey: 'sk_live_M0NdxZIiEIoR3H4aQwRvQP3O'
   },
 
-  google: {
-      serviceAccount: '555184822066-f48i274j6tsj4pl8gs06isua0cb91q7a@developer.gserviceaccount.com',
-      email: 'admin@trainify.io',
-      password: 'NpRzkTRoxih]me[fj#caqKXcsD]]Ld}(h'
-  },
-
   mandrill: 'QhbawI-EVGiLprizwN3i0A',
-
-  server: {
-    port: 6158,
-    session: 'SomeTHINGRand0m',
-    pass: 'R{ZFBAh$=q/YYYTh@Vekc%PDLibv]yaYjzh'
-  },
 
   db: {
     port: 5432,
